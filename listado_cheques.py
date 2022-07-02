905a5bb3d8f83c0a463a1a792f4cd3e849db602e
Ailin Jelenek <jd_ailin@hotmail.com>
2022-07-02 20:32:30 -0300

    Segunda parte

--- listado_cheques.py
+++ listado_cheques.py
@@ -9,3 +9,14 @@ def checkEstado(parametro):
 if len(argumentos) >= 5:
     nombreArchivo, DNI, salida, tipoCheque = argumentos[1], argumentos[2], argumentos[3], argumentos[4]
     
+    if len(argumentos) == 6:
+        if checkEstado(argumentos[5]):
+            estado = argumentos[5]
+        else:
+            rangoFecha= argumentos[5].split(":")
+
+    if len(argumentos) == 7:
+        if checkEstado(argumentos[5]):
+            estado= argumentos[5]
+            rangoFecha= argumentos[6].split(":")
+
